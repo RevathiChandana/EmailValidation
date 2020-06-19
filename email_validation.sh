c65ffbbd0e98a7e0176117c4470e1bb011453bd9
revathi chandana <chandanarevathi30@gmail.com>
2020-06-19 15:26:33 +0530

    uc2:entered first and last name should contains first letter capital and minimum 3 characters required

--- email_validation.sh
+++ email_validation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 shopt -s extglob
 echo "To check Email validation"
-read -p " enter a valid first name : " first_name
-print_valid_First_name()
+read -p " enter first name : " first_name
+read -p "enter last name : " last_name
+print_valid_name()
 {
-        if [[ $1 =~ ^([A-Z]{1}+[a-z0-9]{3,})$ ]]
+        if [[ $1 =~ ^([A-Z]{1}+[a-z]{3,})$ ]]
         then
-                echo "first name is validate."
-        else
-                echo "first name is not validate."
+		echo "validate"
+	else
+		echo "name should start with capital and contains minimum 3 characters"
         fi
 }
-print_valid_First_name $first_name
+print_valid_name $first_name
+print_valid_name $last_name
